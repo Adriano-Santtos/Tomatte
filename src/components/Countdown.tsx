444c1c9f50ffc57d271d5c90d1a16ef36d4debdd
Adriano Santtos <adrianosantosx2@hotmail.com>
2021-02-24 21:47:30 -0300

    contextAPI added

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -1,10 +1,16 @@
-import { useEffect, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContex';
 import styles from '../styles/components/Countdown.module.css'
 
-export function Countdown (){
+let countdownTimeout: NodeJS.Timeout;
 
-    const [time, setTime] = useState(25*60);
-    const [active, setActive] = useState(false);
+export function Countdown (){
+    const{startNewChallenge} = useContext(ChallengesContext);
+
+    const [time, setTime] = useState(0.05*60);
+    const [isActive, setIsActive] = useState(false);
+    const [hasFinished, setHasFinished] = useState(false);
+
 
     const minutes = Math.floor(time/60);
     const seconds = time%60;
@@ -13,16 +19,26 @@ export function Countdown (){
     const [sencondLeft, secondRight] = String(seconds).padStart(2, '0').split('');
 
     function startCountdown(){
-        setActive(true);
+        setIsActive(true);
+    }
+
+    function resetCountdown(){
+        clearTimeout(countdownTimeout); //JS puro
+        setIsActive(false);
+        setTime(0.1*60);
     }
 
     useEffect(() => {
-        if(active && time > 0){
-            setTimeout(()=>{
+        if(isActive && time > 0){
+           countdownTimeout = setTimeout(()=> {
                 setTime(time-1);;
             }, 1000)
+        }else if (isActive && time === 0){
+            setHasFinished(true);
+            setIsActive(false);
+            startNewChallenge();
         }
-    },[active, time]);
+    },[isActive, time]);
 
     return(
         <div>
@@ -38,13 +54,36 @@ export function Countdown (){
                         <span>{secondRight} </span>
                     </div>
                 </div>
+                
+                {hasFinished ? (
+                     <button 
+                     disabled
+                     className={styles.countdownButton}
+                     >
+                        Ciclo encerrado
+                     </button>
+                ):(
+                    <>
+                    {isActive ? (
+                         <button 
+                         type="button" 
+                         className={`${styles.countdownButton} ${styles.countdownButtonActive}`}
+                         onClick={resetCountdown}
+                         >
+                         Abandonar ciclo
+                         </button>)
+                  : (
+                        <button 
+                        type="button" 
+                        className={styles.countdownButton}
+                        onClick={startCountdown}
+                         >
+                            Iniciar Ciclo
+                        </button>
 
-                <button 
-                type="button" 
-                className={styles.countdownButton}
-                onClick={startCountdown}>
-                    Iniciar um Ciclo
-                </button>
+                    )}
+                    </>
+                 )}
         </div>
     );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,11 @@
+import Head from 'next/head';
 
 import { CompletedChallenges } from "../components/CompletedChallenges";
 import { Countdown } from "../components/Countdown";
 import { ExperienceBar } from "../components/ExperienceBar";
 import {Profile} from "../components/Profile";
+import { ChallengeBox } from "../components/ChallengeBox";
 
-import Head from 'next/head';
 
 import styles from '../styles/pages/Home.module.css';
 
@@ -16,17 +17,20 @@ export default function Home() {
       </Head>
 
       <ExperienceBar/>
-      
+
       <section>
+
         <div>
-          <Profile/>
+        <Profile/>
          <CompletedChallenges/>
          <Countdown/>
         </div>
-        <div></div>
+
+        <div>
+          <ChallengeBox/>
+        </div>
+        
       </section>
     </div>
-
-
-  )
+  );
 }
