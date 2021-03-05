dee390190833d3b35727f9135cc6c77104f5f070
Adriano Santtos <adrianosantosx2@hotmail.com>
2021-03-05 20:46:10 -0300

    starting add auth

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,16 +1,19 @@
 
-import { FiMoon } from 'react-icons/fi';
+import { FiMoon, FiBell} from 'react-icons/fi';
 import styles from '../styles/components/Sidebar.module.css';
 
+
 export function Sidebar() {
-  return (
+
+  return(
 
     <div className={styles.container}>
 
       <img src="/logo-full.svg" alt=""/>
 
       <div className={styles.buttons}>
-          
+        
+       
 
         <button type="button">
           <div></div>
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,13 @@
 import '../styles/global.css';
 
-import {ChallengesProvider} from '../contexts/ChallengesContext';
-
+import { ThemeProvider } from 'next-themes'
 
 function MyApp({ Component, pageProps }) {
-  return(
-     <Component {...pageProps} />
+  return (
+    <ThemeProvider>
+      <Component {...pageProps} />
+    </ThemeProvider>
   )
-  
 }
 
 export default MyApp
